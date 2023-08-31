5b8f9b6ab4761dee007f032750806bf2448e0eb4
rangat0102@gmail.com <rangat0102@gmail.com>
2023-08-31 04:05:18 +0000

    ed

--- models/sample.py
+++ models/sample.py
@@ -1,5 +1,6 @@
 import snowflake.snowpark.functions as F
-def model(dbt,session):    
+
+def model(dbt, session):
     dbt.config(materialized="table")
     dept_1 = dbt.ref("dept_model")
     return dept_1
