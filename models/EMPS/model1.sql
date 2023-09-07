17fd75db5c4ddf8e7c47e04a75ac85d8343217dd
Ranga T <rangat0102@gmail.com>
2023-09-07 23:48:59 +0000

    sdfsdfds

--- models/EMPS/model1.sql
+++ models/EMPS/model1.sql
@@ -6,6 +6,6 @@ with cte1 as (
      select DEPTNO  ,JOB as jobskkk, sum(sal) from A.public.EMP
 group by DEPTNO, JOB 
 )
-
-select * from cte1
+,cte2 as ( select * From cte1 where DEPTNO = 10)
+select * from cte2
  
