0cff024cd0430020f51b254582551f0bfe1506a6
Ranga T <rangat0102@gmail.com>
2023-09-08 13:31:58 +0000

    new code

--- models/EMPS/join1.sql
+++ models/EMPS/join1.sql
@@ -9,3 +9,6 @@ select * From {{ref('emp_model')}}
 ( select DNAME, ENAME, HIREDATE, SAL, COMM from emp_ref inner join dept_ref on dept_ref.DEPTNOD = emp_ref.DEPTNOE ) 
 select DNAME, ENAME, HIREDATE, SAL, COMM From join_cte
 
+
+
+-----------------khguyh------------------
